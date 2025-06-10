ef426c279de50f37085cefbc96ddca007de7c3b7
Vidya236 <nvidyavallika6@gmail.com>
2025-06-10 15:17:43 +0530

    added some more entities

--- CRM_PROJECT/Ticket.java
+++ CRM_PROJECT/Ticket.java
@@ -6,7 +6,9 @@ public class Ticket {
         int customerId = 1744368;
         String issueType = "Billing";
         String issueDescription = "Payment failed";
+        char status = 'N'; // N for New, P for Pending, R for Resolved, C
         byte priority = 2;
+        float customerRated = 2.8f;
         boolean isOpen = true;
         String customerNotes = "Customer is not satisfied with the service";
         // printing the values
@@ -14,7 +16,9 @@ public class Ticket {
         System.out.println("Customer ID: " + customerId);
         System.out.println("Issue Type: " + issueType);
         System.out.println("Issue Description: " + issueDescription);
+        System.out.println("Status: " + status);
         System.out.println("Priority: " + priority);
+        System.out.println("Customer Rated: " + customerRated);
         System.out.println("Is Open: " + isOpen);
         System.out.println("Customer Notes: " + customerNotes);
     }
