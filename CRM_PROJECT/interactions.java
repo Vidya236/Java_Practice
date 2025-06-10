230cbd3f43c9fc4cb160b1a907026b7ab2996008
Vidya236 <nvidyavallika6@gmail.com>
2025-06-10 13:58:10 +0530

    Interaction details

---
+++ CRM_PROJECT/interactions.java
@@ -0,0 +1,30 @@
+package CRM_PROJECT;
+
+public class interactions {
+    public static void main(String[] args) {
+        int interactionId = 31119;
+        int customerId = 1743744;
+        short leadId = 2445;
+        String interactionType = "Phone";
+        String interactionStatus = "Completed";
+        boolean isCompleted = true;
+        String interactionDate = "2022-01-01";
+        short interactionInMinutes = 30;
+        float interactionRating = 3.5f;
+        String interactionDescription = "Test interaction";
+        String Notes = "cbiidfdgwyfdqwdehwbd.leifdwscdwghs dwsdwtrdwgdjwdhiwd6wcd ws,kqpsqytsdqgfsc basb";
+        // printing the values
+        System.out.println("Interaction ID: " + interactionId);
+        System.out.println("Customer ID: " + customerId);
+        System.out.println("Lead ID: " + leadId);
+        System.out.println("Interaction Type: " + interactionType);
+        System.out.println("Interaction Status: " + interactionStatus);
+        System.out.println("Is Completed: " + isCompleted);
+        System.out.println("Interaction Date: " + interactionDate);
+        System.out.println("Interaction In Minutes: " + interactionInMinutes);
+        System.out.println("Interaction Rating: " + interactionRating);
+        System.out.println("Interaction Description: " + interactionDescription);
+        System.out.println("Notes: " + Notes);
+    }
+
+}
