00071df6309d435cd52645ff3c2fd65332af2969
Vidya236 <nvidyavallika6@gmail.com>
2025-06-10 12:25:48 +0530

    Trainer Details

---
+++ LMS_PROJECT/Trainer.java
@@ -0,0 +1,34 @@
+package LMS_PROJECT;
+
+public class Trainer {
+    public static void main(String[] args) {
+        short TrainerId = 765;
+        String TrainerName = "Ravi";
+        byte TrainerAge = 35;
+        char TrainerGender = 'M';
+        byte TrainerExperience = 12;
+        long TrainerContact = 9876543210l;
+        String TrainerEmail = "ravi@gmail.com";
+        String TrainerAddress = "Bangalore";
+        String TrainerSpecialization = "Java";
+        String Qualification = "M.Tech";
+        int Salary = 50000;
+        float TrainerRating = 4.5f;
+
+        // Printing Values
+        System.out.println("Trainer ID: " + TrainerId);
+        System.out.println("Trainer Name: " + TrainerName);
+        System.out.println("TrainerAge: " + TrainerAge);
+        System.out.println("Trainer Gender: " + TrainerGender);
+        System.out.println("Trainer Experience: " + TrainerExperience);
+        System.out.println("Trainer Contact: " + TrainerContact);
+        System.out.println("Trainer Email: " + TrainerEmail);
+        System.out.println("Trainer Address: " + TrainerAddress);
+        System.out.println("Trainer Specialization: " + TrainerSpecialization);
+        System.out.println("Qualification: " + Qualification);
+        System.out.println("Salary: " + Salary);
+        System.out.println("Trainer Rating: " + TrainerRating);
+
+    }
+
+}
