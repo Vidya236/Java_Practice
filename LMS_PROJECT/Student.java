c6dc552e07c39412539b70c7af9161528bfb6aed
Vidya236 <nvidyavallika6@gmail.com>
2025-06-10 12:11:18 +0530

    Student Details

---
+++ LMS_PROJECT/Student.java
@@ -0,0 +1,32 @@
+package LMS_PROJECT;
+
+public class Student {
+    public static void main(String[] args) {
+        short studentId = 1021;
+        String studentName = "John Doe";
+        byte age = 20;
+        char gender = 'M';
+        String courseName = "Java Full Stack Development";
+        byte courseId = 101;
+        String email = "john.doe@lms.com";
+        int phone = 1234567890;
+        String address = "123 Main St, Anytown, USA";
+        String studentStatus = "Active";
+        float studentRating = 4.2f;
+        boolean isInstructor = false;
+        // printing values
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Age: " + age);
+        System.out.println("Gender: " + gender);
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Course ID: " + courseId);
+        System.out.println("Email: " + email);
+        System.out.println("Phone: " + phone);
+        System.out.println("Address: " + address);
+        System.out.println("Student Status: " + studentStatus);
+        System.out.println("Student Rating: " + studentRating);
+        System.out.println("Is trainer: " + isInstructor);
+    }
+
+}
