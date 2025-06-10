7c012e2edf179da68d3d785f11b6d67894d13802
Vidya236 <nvidyavallika6@gmail.com>
2025-06-10 15:53:37 +0530

    Application Form Details

---
+++ APPLICATION_FORM/Form.java
@@ -0,0 +1,65 @@
+package APPLICATION_FORM;
+
+public class Form {
+    public static void main(String[] args) {
+        // String appName= "Application For College Admission Test";
+        String UniName = "Panagasinan Sate University";
+        byte year = 1;
+        boolean isTransferee = false;
+        boolean isReturnee = false;
+        String LastName = "Nanda";
+        String FirstName = "Rizal";
+        String MiddleName = "Jr.";
+        String Address = "Poblacion, San Nicolas, Pangasinan";
+        short yearOfBirth = 2004;
+        byte age = 21;
+        String BirthPlace = "Filipino";
+        char citizenship = 'f'; // f- filipino or o-others
+        char sex = 'M';
+        char civilStatus = 's'; // s-single or m-married
+        long contactNumber = 9846218257L;
+        String email = "rizal.nanda@gmail.com";
+        String srHighScl = "St.johns High School";
+        float gpa = 7.6f;
+        short gradYear = 2019;
+        String fatherName = "Suyash Nanda";
+        String motherName = "Sunita Nanda";
+        String Occupation = "Business-Man";
+        int salaryMonthly = 300000;
+        boolean isASL = true;
+        boolean physicallyDisabled = false;
+        String disabilityType = "None";
+        String Signature = "Rizal Nanda";
+        // Printing the values
+        System.out.println("Application For College Admission Test");
+        System.out.println("University Name: " + UniName);
+        System.out.println("Year: " + year);
+        System.out.println("Is Transferee: " + isTransferee);
+        System.out.println("Is Returnee: " + isReturnee);
+        System.out.println("Last Name: " + LastName);
+        System.out.println("First Name: " + FirstName);
+        System.out.println("Middle Name: " + MiddleName);
+        System.out.println("Address: " + Address);
+        System.out.println("Birth Year: " + yearOfBirth);
+        System.out.println("Age: " + age);
+        System.out.println("Birth Place: " + BirthPlace);
+        System.out.println("Citizenship: " + citizenship);
+        System.out.println("Sex: " + sex);
+        System.out.println("Civil Status: " + civilStatus);
+        System.out.println("Contact Number: " + contactNumber);
+        System.out.println("email: " + email);
+        System.out.println("Senior High School: " + srHighScl);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Graduation Year: " + gradYear);
+        System.out.println("Father's Name: " + fatherName);
+        System.out.println("Mother Name: " + motherName);
+        System.out.println("Occupation: " + Occupation);
+        System.out.println("Salary Monthly: " + salaryMonthly);
+        System.out.println(" is ASL Passer: " + isASL);
+        System.out.println(" Physically Disabled: " + physicallyDisabled);
+        System.out.println("Dissability Type: " + disabilityType);
+        System.out.println("Signature: " + Signature);
+
+    }
+
+}
