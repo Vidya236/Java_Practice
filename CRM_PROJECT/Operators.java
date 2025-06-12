28fe095c329668bdd5a5b4a6da98678b3c9d29aa
Vidya236 <nvidyavallika6@gmail.com>
2025-06-12 14:14:21 +0530

    operators on crm

---
+++ CRM_PROJECT/Operators.java
@@ -0,0 +1,26 @@
+package CRM_PROJECT;
+
+public class Operators {
+    public static void main(String[] args) {
+
+        int customerId = 1234;
+        String customerName = "john";
+        double totalPurchase = 4500.0;
+        int interactionCount = 45;
+        int leadId = 345;
+        double leadBudget = 2500;
+        boolean isContacted = true;
+        double opportunityValue = 4563;
+        boolean isClosed = false;
+
+        double avgPurchases = totalPurchase / interactionCount;
+        boolean saleopp = opportunityValue > 5000;
+        boolean isPrioritised = leadBudget > 3000 && isContacted;
+        interactionCount++;
+
+        System.out.println("Average Purchases: " + avgPurchases);
+        System.out.println("Sales opportunity: " + saleopp);
+        System.out.println("Is Prioritised: " + isPrioritised);
+        System.out.println("Interaction Count after increment: " + interactionCount);
+    }
+}
